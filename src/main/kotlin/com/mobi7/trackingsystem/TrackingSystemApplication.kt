1a50170038cfaff861c1f212c06b12c508627f57
mariel <coutomariel@gmail.com>
2023-09-25 11:18:43 -0300

    Initial commit

---
+++ src/main/kotlin/com/mobi7/trackingsystem/TrackingSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.mobi7.trackingsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TrackingSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<TrackingSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/mobi7/trackingsystem/TrackingSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mobi7.trackingsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TrackingSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
